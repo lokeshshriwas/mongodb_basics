f078c0da07a22e1b09831839f8f003a1f0e77aff
lokesh shriwas <lokeshshriwas2802@gmail.com>
2023-11-18 21:44:20 +0530

    contraints in schema (validations)

---
+++ books.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose")
+
+main()
+.then(res=> console.log("connection stablised"))
+.catch((err)=> console.log(err))
+
+
+async function main(){
+    await mongoose.connect('mongodb://127.0.0.1:27017/amazon');
+}
+
+const bookschema = new mongoose.Schema({
+    title : {
+        type: String,
+        required : true
+    },
+    author : {
+        type: String,
+    },
+    price : {
+        type: Number,
+        min: [1, "smaller than minimum selling price"]
+    },
+    category : {
+        type: String,
+        enum: ["friction", "nonfriction", "selfhelp"]
+    },
+    discount: {
+        type: Number,
+        default : 0
+    }
+})
+
+const Book = mongoose.model("Book", bookschema)
+
+Book.create({
+    title: "psychology of money",
+    author: "lokesh",
+    price: 99,
+    category: "selfhelp"
+}). then(res=> console.log(res))
+.catch((err)=> console.log(err))
